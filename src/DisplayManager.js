8881885307b3083819b147f1525563a38504956d
Josh Yeaton <yeaton.josh@gmail.com>
2025-06-24 16:50:12 -0400

    Update display manager api

--- src/DisplayManager.js
+++ src/DisplayManager.js
@@ -3,20 +3,26 @@ export default class DisplayManager {
         this.activeProject = null;
     }
 
-    redrawActiveTasks() {
+    redrawActiveTasks(displayProjectName, taskList) {
         // Called when we apply a filter of some sort to redraw the main task window
+        // displayProjectName should either be true or false depending on whether we're looking at all tasks
+        // tasklist should simply be an array of tasks
     }
 
-    updateTask(projectId, taskId) {
-        // Called when we want to redraw a task without expanding or collapsing (for example, toggling complete)
+    redrawSingleTask(taskId, task) {
+        // Called when we want to redraw a task without expanding or collapsing (for example, toggling complete). Might be called after collapsing
     }
 
-    expandTask(projectId, taskId) {
+    expandTask(taskId, task) {
         // Called when we want to view the details of or edit a task
     }
 
-    collapseTask(projectId, taskId) {
+    collapseTask(taskId, task) {
         // Called when we are done editing the details of a task
+    }
+
+    appendTask(taskId, projectId, task) {
+        // appends a task to the end of the active tasks
     }
 
     expandAddTask() {
@@ -26,5 +32,9 @@ export default class DisplayManager {
     collapseAddTask() {
         // Called when we are done adding a task at the bottom of the screen
 
+    }
+
+    redrawProjectSidebar(projects) {
+        // Called to redraw the project sidebar
     }
 }
